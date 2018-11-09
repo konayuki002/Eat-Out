e2667cec7744dce4aba5af49204b4be68da34ff4
WAFER\konay <konayuki0455@gmail.com>
2018-11-09 23:54:53 +0900

    made

--- app/src/main/java/com/example/konay/eatout/MainActivity.kt
+++ app/src/main/java/com/example/konay/eatout/MainActivity.kt
@@ -4,15 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+import java.util.Random
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var cookie = 0
+        val restrant = arrayOf("つけ麺凌駕", "らーめん凌駕", "こころ", "マクドナルド", "婆娑羅", "四川屋台", "新源", "サイゼリヤ", "一食", "二食", "歌志軒", "王将", "CoCo壱", "昼顔", "たんや", "松屋")
         button.setOnClickListener {
-            cookie++
-            textview.text = cookie.toString()
+            val rand = Random()
+            var index = rand.nextInt(restrant.size)
+            textview.text=restrant[index]
         }
     }
 
