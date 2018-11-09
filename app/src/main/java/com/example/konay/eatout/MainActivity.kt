d5b523c7d6aa2b5fe96210484c00235e7c72996d
WAFER\konay <konayuki0455@gmail.com>
2018-11-09 23:59:52 +0900

    fixed a bit

--- app/src/main/java/com/example/konay/eatout/MainActivity.kt
+++ app/src/main/java/com/example/konay/eatout/MainActivity.kt
@@ -7,16 +7,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
-
+    val random = Random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val restrant = arrayOf("つけ麺凌駕", "らーめん凌駕", "こころ", "マクドナルド", "婆娑羅", "四川屋台", "新源", "サイゼリヤ", "一食", "二食", "歌志軒", "王将", "CoCo壱", "昼顔", "たんや", "松屋")
+        val restaurant = arrayOf("つけ麺凌駕", "らーめん凌駕", "こころ", "マクドナルド", "婆娑羅", "四川屋台", "新源", "サイゼリヤ", "一食", "二食", "歌志軒", "王将", "CoCo壱", "昼顔", "たんや", "松屋")
         button.setOnClickListener {
-            val rand = Random()
-            var index = rand.nextInt(restrant.size)
-            textview.text=restrant[index]
+            var index = rand(0,restaurant.size)
+            textview.text=restaurant[index]
         }
+    }
+    fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
     }
 
 }
